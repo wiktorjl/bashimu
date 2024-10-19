2fc836516ccf7b4eecbca2cf37b875cc2f16422e
Wiktor Lukasik <contact@wiktor.io>
2024-10-19 14:10:20 -0700

    Simplified deploy script

--- bashimu_deploy.sh
+++ bashimu_deploy.sh
@@ -14,16 +14,24 @@ echo  "Welcome to Bashimu! A simple script to interact with OpenAI's API for bas
 echo  "This script will send your question to the OpenAI API and return a concise command line answer."
 echo  "Please follow the prompts to configure your environment." 
 echo 
-# Generate a random filename in /tmp
-sh_file="/tmp/bashimu_$(date +%s).sh"
 
 # Download the bashimu.sh script to /tmp
+sh_file="/tmp/bashimu_$(date +%s).sh"
+echo "Downloading bashimu.sh script to $sh_file"
 curl -s https://raw.githubusercontent.com/wiktorjl/bashimu/main/bashimu.sh > $sh_file 2>/dev/null 
 
-# Same for bashimu_setup.sh
-setup_file="/tmp/bashimu_setup_$(date +%s).sh"
-curl -s https://raw.githubusercontent.com/wiktorjl/bashimu/main/bashimu_setup.sh > $setup_file 2>/dev/null
+echo "Moving the bashimu.sh script to /usr/local/bin"
+sudo mv $sh_file /usr/local/bin/bashimu.sh
+chmod +x /usr/local/bin/bashimu.sh
 
-# Run the setup script
-bash $setup_file $sh_file
+echo "Creating ~/.bashimurc file"
+echo "#!/bin/sh" > ~/.bashimurc
+echo "export OPENAI_API_KEY=\"\"" >> ~/.bashimurc
+echo "export OPENAI_MODEL_NAME=\"gpt-4o-mini\"" >> ~/.bashimurc
+echo "alias ?=\"/usr/local/bin/bashimu\"" >> ~/.bashimurc
 
+echo "Adding source ~/.bashimurc to ~/.bashrc"
+echo "source ~/.bashimurc" >> ~/.bashrc
+
+echo "Done! Please open a new terminal to start using Bashimu."
+echo "Or you can run 'source ~/.bashrc' to start using it in the current terminal."
